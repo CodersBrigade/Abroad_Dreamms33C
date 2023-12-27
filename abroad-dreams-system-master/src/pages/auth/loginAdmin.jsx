c86dd417291361863b86a20f9cf0bcc42fbd4acd
Karanbohara01 <karanbohara216@gmail.com>
2023-12-27 12:29:15 +0545

    admin-login-page-css
    -css added

--- abroad-dreams-system-master/src/pages/auth/loginAdmin.jsx
+++ abroad-dreams-system-master/src/pages/auth/loginAdmin.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import { Container, Row, Col, Form, Button, Alert } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
+// import of 'bootstrap/dist/css/bootstrap.min.css';
+import './loginAdmin.css'
 
 function LoginAdmin() {
   const [email, setEmail] = useState('');
@@ -22,53 +23,46 @@ function LoginAdmin() {
   };
 
   return (
-      <div className="Login" style={{ marginTop: 100 }}>
-        <Container className="mt-5" style={{
-          backgroundColor: '#fcfcfc',
-          borderWidth: 2,
-          borderColor: 'black',
-          borderStyle: 'solid',
-          width: 900,
-          padding: 40,
-          paddingTop: 80,
-          paddingBottom: 80,
-          borderRadius: 20,
-        }}>
-          <Row>
-            <Col md={{ span: 6, offset: 3 }}>
-              <Form onSubmit={handleSubmit}>
-                {error && <Alert variant="danger">{error}</Alert>}
+    <div className="Login">
+      <Row>
+        <Col>
+          <Form onSubmit={handleSubmit}>
+            {error && <Alert variant="danger">{error}</Alert>}
 
-                <Form.Group controlId="formBasicEmail">
-                  <Form.Label>Email Address</Form.Label>
-                  <Form.Control
-                      type="email"
-                      placeholder="Your Email Address"
-                      value={email}
-                      onChange={(e) => setEmail(e.target.value)}
-                  />
-                </Form.Group>
+            <Form.Group controlId="formBasicEmail">
+              <h3>Login</h3>
+              <p>Welcome back!</p>
+              <Form.Label style={{fontWeight:"bold",color:"green",marginLeft:125, marginBottom:20}} >Email Address</Form.Label>
 
-                <br />
+              <Form.Control
+                type="email"
+                placeholder="Your Email Address"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </Form.Group>
+            <br />
 
-                <Form.Group controlId="formBasicPassword">
-                  <Form.Label>Password</Form.Label>
-                  <Form.Control
-                      type="password"
-                      placeholder="Your Password"
-                      value={password}
-                      onChange={(e) => setPassword(e.target.value)}
-                  />
-                </Form.Group>
-
-                <Button className="btn btn-primary" style={{ width: 150, marginTop: 30, backgroundColor:"green", borderColor:"green", }} variant="primary" type="submit">
-                  Login
-                </Button>
-              </Form>
-            </Col>
-          </Row>
-        </Container>
-      </div>
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label style={{fontWeight:"bold",color:"green",marginLeft:140,marginBottom:20}}>Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Your Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </Form.Group>
+            <Button
+              className="btn btn-primary"
+              variant="primary"
+              type="submit"
+            >
+              Login
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+    </div>
   );
 }
 
