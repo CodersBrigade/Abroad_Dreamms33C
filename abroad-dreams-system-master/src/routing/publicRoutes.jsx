9438f0d028d05253a9355cf2c8114ac31c0824a6
SushmitaBiswakarma <biswakarmasushmita5@gmail.com>
2024-01-17 14:51:50 +0545

    Forgot Password Frontend
    -- Navigate to the new page
    -- frontend for reset email

--- abroad-dreams-system-master/src/routing/publicRoutes.jsx
+++ abroad-dreams-system-master/src/routing/publicRoutes.jsx
@@ -6,13 +6,17 @@ import Login from '../pages/login/Login.jsx';
 import Home from '../pages/home/Home.jsx';
 import PageNotFound from "../pages/PageNotFound.jsx";
 import AllCourses from "../pages/home/AllCourses.jsx";
+import ForgotPassword from "../pages/login/forgotpassword.jsx";
 
 const publicRoutes = createBrowserRouter([
     {path: '/', element: <Home/>},
     {path: '/login', element: <Login/>},
     {path: '/student/sign-up', element: <StudentRegister/>},
     {path: '/allCourses', element: <AllCourses/>},
+    {path: '*', element: <PageNotFound/>},
+    {path: '/forgot-password', element: <ForgotPassword/>},
     {path: '*', element: <PageNotFound/>}
+
 
 
 
