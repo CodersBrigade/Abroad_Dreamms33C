a2c84123f798330f97d51faca281b79d047fd37a
Karanbohara01 <karanbohara216@gmail.com>
2024-01-24 23:37:28 +0545

    css added to student sidebar
    css added

--- abroad-dreams-system-master/src/pages/student/StudentDashboard/StudentSidebar.jsx
+++ abroad-dreams-system-master/src/pages/student/StudentDashboard/StudentSidebar.jsx
@@ -6,14 +6,17 @@ import {
   FaBook,
   FaCog,
   FaBell,
-  FaSignOutAlt, FaSchool, FaBookReader, FaChalkboardTeacher,
+  FaSignOutAlt,
+  FaSchool,
+  FaBookReader,
+  FaChalkboardTeacher,
 } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import "./StudentSidebar.css";
-import {IoIosPaper} from "react-icons/io";
-import {MdDateRange, MdEditNotifications} from "react-icons/md";
-import {BsCashStack} from "react-icons/bs";
-import {BiSolidReport} from "react-icons/bi";
+import { IoIosPaper } from "react-icons/io";
+import { MdDateRange, MdEditNotifications } from "react-icons/md";
+import { BsCashStack } from "react-icons/bs";
+import { BiSolidReport } from "react-icons/bi";
 
 const StudentSidebar = () => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
@@ -31,58 +34,80 @@ const StudentSidebar = () => {
     return location.pathname === path;
   };
 
-
   return (
-      <div className="sidebar">
-        <div className="sidebarWrapper">
-          <div className="sidebarMenu">
-            <h3 className="sidebarTitle">Dashboard</h3>
-            <ul className="sidebarList">
-              <li className={`sidebarListItem ${isLinkActive("/student/dashboard") ? "active" : ""}`}>
-                <Link to="/student/dashboard" className="sidebarLink">
-                  <FaHome />
-                  My Dashboard
-                </Link>
-              </li>
-              <li className={`sidebarListItem ${isLinkActive("/student/profile") ? "active" : ""}`}>
-                <Link to="../student/profile" className="sidebarLink">
-                  <IoIosPaper />
-                  My Profile
-                </Link>
-              </li>
-              <li className={`sidebarListItem ${isLinkActive("/student/studentcourses") ? "active" : ""}`}>
-                <Link to="/student/studentcourses" className="sidebarLink">
-                  <MdDateRange />
-                  My Courses
-                </Link>
-              </li>
-              <li className={`sidebarListItem ${isLinkActive("/student/myapplication") ? "active" : ""}`}>
-                <Link to="/student/myapplication" className="sidebarLink">
-                  <BsCashStack />
-                  MyApplications
-                </Link>
-              </li>
-            </ul>
-          </div>
-          <div className="sidebarMenu">
-            <h3 className="sidebarTitle">Major Sections</h3>
-            <li className={`sidebarListItem ${isLinkActive("/student/payment") ? "active" : ""}`}>
-              <Link to="/student/payment" className="sidebarLink">
-                <FaSchool />
-                MyPayments
+    <div className="sidebar">
+      <div className="sidebarWrapper">
+        <div className="sidebarMenu">
+          <h3 className="sidebarTitle">Dashboard</h3>
+          <ul className="sidebarList">
+            <li
+              className={`sidebarListItem ${
+                isLinkActive("/student/dashboard") ? "active" : ""
+              }`}
+            >
+              <Link to="/student/dashboard" className="sidebarLink">
+                <FaHome style={{ marginRight: 10 }} />
+                My Dashboard
               </Link>
             </li>
-
-            <li className={`sidebarListItem ${isLinkActive("/student/notice") ? "active" : ""}`}>
-              <Link to="/student/notice" className="sidebarLink">
-                <FaBookReader />
-                My Notices
+            <li
+              className={`sidebarListItem ${
+                isLinkActive("/student/profile") ? "active" : ""
+              }`}
+            >
+              <Link to="../student/profile" className="sidebarLink">
+                <IoIosPaper style={{ marginRight: 10 }} />
+                My Profile
               </Link>
             </li>
+            <li
+              className={`sidebarListItem ${
+                isLinkActive("/student/studentcourses") ? "active" : ""
+              }`}
+            >
+              <Link to="/student/studentcourses" className="sidebarLink">
+                <MdDateRange style={{ marginRight: 10 }} />
+                My Courses
+              </Link>
+            </li>
+            <li
+              className={`sidebarListItem ${
+                isLinkActive("/student/myapplication") ? "active" : ""
+              }`}
+            >
+              <Link to="/student/myapplication" className="sidebarLink">
+                <BsCashStack style={{ marginRight: 10 }} />
+                MyApplications
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div className="sidebarMenu">
+          <h3 className="sidebarTitle">Major Sections</h3>
+          <li
+            className={`sidebarListItem ${
+              isLinkActive("/student/payment") ? "active" : ""
+            }`}
+          >
+            <Link to="/student/payment" className="sidebarLink">
+              <FaSchool style={{ marginRight: 10 }} />
+              MyPayments
+            </Link>
+          </li>
 
-          </div>
+          <li
+            className={`sidebarListItem ${
+              isLinkActive("/student/notice") ? "active" : ""
+            }`}
+          >
+            <Link to="/student/notice" className="sidebarLink">
+              <FaBookReader style={{ marginRight: 10 }} />
+              My Notices
+            </Link>
+          </li>
         </div>
       </div>
+    </div>
   );
 };
 
