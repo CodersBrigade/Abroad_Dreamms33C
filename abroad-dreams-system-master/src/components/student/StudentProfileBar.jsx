caca5d0502f2a61712cca17e71bbcb6921887c6d
Karanbohara01 <karanbohara216@gmail.com>
2024-01-25 08:01:59 +0545

    changed font color
    profile bar

--- abroad-dreams-system-master/src/components/student/StudentProfileBar.jsx
+++ abroad-dreams-system-master/src/components/student/StudentProfileBar.jsx
@@ -1,27 +1,29 @@
-import React from 'react';
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
-import { FiBell, FiLogOut } from 'react-icons/fi';
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import { FiBell, FiLogOut } from "react-icons/fi";
 
 const StudentProfileBar = () => {
-    const handleLogout = () => {
-        localStorage.clear();
-        window.location.href = '/';
-    };
+  const handleLogout = () => {
+    localStorage.clear();
+    window.location.href = "/";
+  };
 
-    return (
-        <Navbar className="bg-light justify-content-between">
-            <Navbar.Text>Welcome, {localStorage.getItem("email")}</Navbar.Text>
-            <Nav>
-                <Nav.Link href="#" className="mr-3">
-                    <FiBell size={24} />
-                </Nav.Link>
-                <Nav.Link onClick={handleLogout}>
-                    <FiLogOut color={"#820b14"} size={24} /> Logout
-                </Nav.Link>
-            </Nav>
-        </Navbar>
-    );
+  return (
+    <Navbar className="bg-light justify-content-between">
+      <Navbar.Text style={{ color: "black" }}>
+        Welcome, {localStorage.getItem("email")}
+      </Navbar.Text>
+      <Nav>
+        <Nav.Link href="#" className="mr-3">
+          <FiBell size={24} />
+        </Nav.Link>
+        <Nav.Link onClick={handleLogout}>
+          <FiLogOut color={"#820b14"} size={24} /> Logout
+        </Nav.Link>
+      </Nav>
+    </Navbar>
+  );
 };
 
 export default StudentProfileBar;
