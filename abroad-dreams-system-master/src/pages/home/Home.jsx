9c91913af84faca0058d37b680c83f1ae601033c
YanishSingh <singhyanish999@gmail.com>
2024-01-29 20:47:34 +0545

    Added Our Services Section in Home Page.
    -- Created a new Our Services component naming ourservices.jsx
    -- Implemented in Home.jsx file

--- abroad-dreams-system-master/src/pages/home/Home.jsx
+++ abroad-dreams-system-master/src/pages/home/Home.jsx
@@ -9,8 +9,12 @@ import Navbar from "../../components/home/navbar/Navbar.jsx";
 import SessionNavbar from "../../components/home/navbar/SessionNavbar.jsx";
 import CourseCard from "./CourseCard.jsx";
 import AppointmentForm from "../../components/home/AppointmentForm.jsx";
+<<<<<<< Updated upstream
 import SubscribeComponent from "../../components/admin/SubscribeComponent.jsx";
 import ExtraNav from "../../components/home/extranav/ExtraNav.jsx";
+=======
+import Ourservices from "../../components/home/ourservices/ourservices.jsx";
+>>>>>>> Stashed changes
 
 const Home = () => {
     const cards = useLoaderData();
@@ -22,10 +26,15 @@ const Home = () => {
         <div>
             <ExtraNav />
             {isSessionActive ? <SessionNavbar /> : <Navbar />} {/* Conditional rendering based on accessToken */}
+            {/* Green background section */}
             <Banner />
             <CourseCard/>
+<<<<<<< Updated upstream
             {/*<SubscribeComponent/>*/}
             <AboutUs />
+=======
+            <Ourservices/>
+>>>>>>> Stashed changes
             <AppointmentForm/>
             <Footer />
         </div>
